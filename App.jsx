86dbf45d5e03c881664924493d7524ba8216d4f0
Pranaov <pranaov5925@gmail.com>
2025-10-07 20:21:55 +0530

    Added hold dice logic

--- App.jsx
+++ App.jsx
@@ -16,7 +16,19 @@ export default function App() {
   }
 
   function rollDice() {
-      setDice(generateAllNewDice())
+      setDice(oldDice => oldDice.map(die => 
+          die.isHeld ?
+              die :
+              { ...die, value: Math.ceil(Math.random() * 6) }
+      ))
+  }
+
+  function hold(id) {
+      setDice(oldDice => oldDice.map(die =>
+          die.id === id ?
+              { ...die, isHeld: !die.isHeld } :
+              die
+      ))
   }
 
   const diceElements = dice.map((dieObj) => (
@@ -24,6 +36,7 @@ export default function App() {
       key={dieObj.id} 
       value={dieObj.value}
       isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
     />
   ))
 
